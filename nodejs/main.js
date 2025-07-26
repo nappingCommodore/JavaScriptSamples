8a89b9f75957399ab00fbbd3061d93fabb88425d
Nitish Singh <niks.sbg@gmail.com>
2025-07-26 18:29:46 +0530

    Updated cors and port settings for express and ajax

--- nodejs/main.js
+++ nodejs/main.js
@@ -1,10 +1,16 @@
 // A GET endpoint to return a simple JSON response in express nodejs
 const express = require('express');
+const cors = require('cors');
 const app = express();
-const PORT_EXPRESS = 4000;
+const PORT_EXPRESS = 3000;
+
+// CORS enablement 
+app.use(cors());
+
 app.get('/api', (req, res) => {
     res.json({ message: 'Hello World from Express.js!' });
 });
+
 app.listen(PORT_EXPRESS, () => {
     console.log(`Express server is running on http://localhost:${PORT_EXPRESS}`);
 });
