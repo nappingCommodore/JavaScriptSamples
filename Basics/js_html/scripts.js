f659dc128b27dafb6c464970fbc7fddc01023c57
nappingCommodore <niks.sbg@gmail.com>
2025-08-09 18:20:52 +0530

    Sync latest changes

--- Basics/js_html/scripts.js
+++ Basics/js_html/scripts.js
@@ -1,4 +1,7 @@
 function handleCLick() {
     console.log("Clicked!");
-    alert("Hello World!");
+   // alert("Hello World from script file!");
+   prompt("Agree to the terms and conditions?"); // Example of a confirmation dialog
+   var url = "https://www.example.com?query='example text'&name=test";
+   console.log(encodeURI(url)); // Encoding the URL
 }
